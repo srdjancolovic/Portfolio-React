efa0ed3b70f81560f61c0938337eea02af527004
Srdjan Colovic <srdjan.colovic@etondigital.com>
2023-04-27 09:09:04 +0200

    updates

--- src/components/Copyright/Copyright.js
+++ src/components/Copyright/Copyright.js
@@ -2,17 +2,17 @@ import { SiReact } from 'react-icons/si';
 import classes from './Copyright.module.scss';
 
 const Copyright = () => {
-  return (
-    <div className={`${classes.copyright} ${classes.container}`}>
-      <p>
-        &#169;2022.
-        <span>
-          <SiReact />
-        </span>
-        Portfolio Website
-      </p>
-      <p>Website by Srdjan Colovic</p>
-    </div>
-  );
+    return (
+        <div className={`${classes.copyright} ${classes.container}`}>
+            <p>
+                &#169;2023.
+                <span>
+                    <SiReact />
+                </span>
+                Portfolio Website
+            </p>
+            <p>Website by Srdjan Colovic</p>
+        </div>
+    );
 };
 export default Copyright;
